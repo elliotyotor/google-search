842a8b0893cb7d1098795bc0f55c9243d625b7c1
elliotyotor <elliotyotor@gmail.com>
2022-09-03 02:44:19 +0300

    filter

--- findbydomain.py
+++ findbydomain.py
@@ -1,12 +1,13 @@
 
 from googlesearch import search
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--n', type=int, required=True)
 parser.add_argument('--q', type=str, required=True)
 args = parser.parse_args()
 f = open("result.txt", "w")
-for i in search(args.q,num=args.n, stop=args.n, pause=2):
-    print(i)
-    f.write(i+"\n")
+for i in search(args.q, stop=args.n, pause=2):
+    if args.q in i:
+        f.write(i+"\n")
 f.close()
